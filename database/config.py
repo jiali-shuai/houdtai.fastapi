27d296af24d25fd4df6adcadf826f95df213e7c9
jiali-shuai <1613014719@qq.com>
2026-03-07 20:52:46 +0800

    first

--- database/config.py
+++ database/config.py
@@ -21,7 +21,7 @@ DATABASE_CONFIG = {
                 "host": "127.0.0.1",
                 "port": 3306,
                 "user": "root",
-                "password": "your_password",
+                "password": "yourpassword",
                 "database": "uer",
             }
         }
